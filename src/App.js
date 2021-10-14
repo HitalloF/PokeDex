4383b94fa5df646e6f2a8bde1048ccaecfa4f6b5
Hitallo Freitas <hitalloufpe@gmail.com>
2021-10-14 16:21:42 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Locations from './Components/Locations';
 import Pokemons from './Components/Pokemons';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 const App = () => {
   return (
     <div>
-      <Pokemons />
-      <Locations />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="pokemons" element={<Pokemons />} />
+          <Route path="locations" element={<Locations />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
